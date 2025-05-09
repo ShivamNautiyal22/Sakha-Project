2f75ce32c9671cb5716c61b13702ddcc8ad8c2e0
ShivamNautiyal22 <snautiyal881@gmail.com>
2025-05-09 19:12:36 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     useEffect(() => {
       const timer = setTimeout(() => {
         setLoading(false);
-      }, 100);
+      }, 4000);
   
       return () => clearTimeout(timer);
     }, []);
